4965f1a8915b4d9e445597ef9c623854bbd8722c
“Jen <jenstewart@gmail.com>
2021-07-07 15:04:32 +0100

    Completed katas 6-7

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,8 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+    const names = namesObj.map(person => person.name).join(", ")// .replace(/,(?=[^,]*$)/, ' &');
+    const lastComma = names.lastIndexOf(","); 
+    const namesStr = names.substr(0, lastComma) + ' &' + names.substr(lastComma + 1); 
+    return namesStr
+};
 
 module.exports = joinNames;
